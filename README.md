0d63cc1da510f431efb87e3262d03c56785e6899
Toby Breckon <toby@breckon.me.uk>
2020-06-23 11:50:34 +0100

    initial versions

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# DoG-saliency
-reference implementation of Real-time Salient Object Detection based on Division of Gaussians [Katramados/Breckon, 2011]
+# Real-time Visual Saliency by Division of Gaussians - Reference Implementation
+
+Tested using Python 3.x.x and [OpenCV 4.x](http://www.opencv.org)
+
+![DOG-Saliency](https://github.com/tobybreckon/DoG-saliency/blob/master/images/example.png)
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Exemplar Real-time Salient Object Detection Using DOG Saliency
+
+## Abstract:
+
+_"....."_
+
+[[Katramados, Breckon, In Proc. International Conference on Image Processing, IEEE, 2011](https://breckon.org/toby/publications/papers/katramados11salient.pdf)]
+
+---
+
+## Reference implementation:
+
+....
+
+
+---
+
+
+---
+
+## Instructions to use:
+
+To download and test the supplied code do:
+
+```
+$ git clone https://github.com/tobybreckon/DoG-saliency.git
+$ cd DoG-saliency
+$ python demo.py ....
+```
+
+where ....
+
+---
+
+## Example video:
+[![Examples](https://github.com/tobybreckon/DoG-saliency/blob/master/images/slic-ex.png)](https://youtu.be/NEED-TO-CREATE)
+Video Example - click image above to play.
+
+---
+
+## References:
+
+If you are making use of this work in any way please reference the following articles in any report, publication, presentation, software release or any other associated materials:
+
+[Real-time Visual Saliency by Division of Gaussians](https://breckon.org/toby/publications/papers/katramados11salient.pdf)
+(Katramados, Breckon), In Proc. International Conference on Image Processing, IEEE, 2011.
+```
+@InProceedings{katramados11salient,
+  author    =    {Katramados, I. and Breckon, T.P.},
+  title     = 	 {Real-time Visual Saliency by Division of Gaussians},
+  booktitle = 	 {Proc. Int. Conf. on Image Processing},
+  pages     = 	 {1741-1744},
+  year      = 	 {2011},
+  month     = 	 {September},
+  publisher =    {IEEE},
+  url       = 	 {https://breckon.org/toby/publications/papers/katramados11salient.pdf},
+  doi       = 	 {10.1109/ICIP.2011.6115785},
+}
+```
+
+For non-commercial use (i.e. academic, non-for-profit and research) the (very permissive) terms of the MIT free software [LICENSE](LICENSE) must be adhered to. For commercial use, the DOG saliency algorithm itself is patented (WIPO reference: [WO2013034878A3](https://patents.google.com/patent/WO2013034878A3/)) and available for licensing via [Cranfield University](https://www.cranfield.ac.uk/).
+
+### Acknowledgements:
+
+XXXXXXXXXXXXXXX, this reference implementation of [Katramados / Breckon, 2011], Durham University, July 2020.
+
+---
---
+++ demo.py
@@ -0,0 +1,145 @@
+##########################################################################
+
+# Example : DoG saliency demo - [Katramados / Breckon 2011]
+
+# This implementation:
+# Copyright (c) 2020 XXXXXXXXXXXXXXX, Toby Breckon, Durham University, UK
+
+##########################################################################
+
+import cv2
+import argparse
+import sys
+import math
+
+##########################################################################
+
+keep_processing = True
+
+# parse command line arguments for camera ID or video file
+
+parser = argparse.ArgumentParser(
+    description='Perform ' +
+    sys.argv[0] +
+    ' example operation on incoming camera/video image')
+parser.add_argument(
+    "-c",
+    "--camera_to_use",
+    type=int,
+    help="specify camera to use",
+    default=0)
+parser.add_argument(
+    "-r",
+    "--rescale",
+    type=float,
+    help="rescale image by this factor",
+    default=1.0)
+parser.add_argument(
+    "-fs",
+    "--fullscreen",
+    action='store_true',
+    help="run in full screen mode")
+parser.add_argument(
+    'video_file',
+    metavar='video_file',
+    type=str,
+    nargs='?',
+    help='specify optional video file')
+args = parser.parse_args()
+
+##########################################################################
+
+# define video capture object
+
+try:
+    # to use a non-buffered camera stream (via a separate thread)
+
+    if not(args.video_file):
+        import camera_stream
+        cap = camera_stream.CameraVideoStream()
+    else:
+        cap = cv2.VideoCapture()  # not needed for video files
+
+except BaseException:
+    # if not then just use OpenCV default
+
+    print("INFO: camera_stream class not found - camera input may be buffered")
+    cap = cv2.VideoCapture()
+
+# define display window name
+
+window_name = "Live Camera Input"  # window name
+
+# if command line arguments are provided try to read video_name
+# otherwise default to capture from attached camera
+
+if (((args.video_file) and (cap.open(str(args.video_file))))
+        or (cap.open(args.camera_to_use))):
+
+    # create window by name (as resizable)
+
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+
+    while (keep_processing):
+
+        # start a timer (to see how long processing and display takes)
+
+        start_t = cv2.getTickCount()
+
+        # if camera /video file successfully open then read frame
+
+        if (cap.isOpened):
+            ret, frame = cap.read()
+
+            # when we reach the end of the video (file) exit cleanly
+
+            if (ret == 0):
+                keep_processing = False
+                continue
+
+            # rescale if specified
+
+            if (args.rescale != 1.0):
+                frame = cv2.resize(
+                    frame, (0, 0), fx=args.rescale, fy=args.rescale)
+
+        # ***
+        # *** do any processing here ****
+        # ***
+
+        # display image
+
+        cv2.imshow(window_name, frame)
+        cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
+                              cv2.WINDOW_FULLSCREEN & args.fullscreen)
+
+        # stop the timer and convert to ms. (to see how long processing and
+        # display takes)
+
+        stop_t = ((cv2.getTickCount() - start_t) /
+                  cv2.getTickFrequency()) * 1000
+
+        # start the event loop + wait 40ms or less depending on
+        # processing time taken (i.e. 1000ms / 25 fps = 40 ms)
+
+        key = cv2.waitKey(max(2, 40 - int(math.ceil(stop_t)))) & 0xFF
+
+        # It can also be set to detect specific key strokes by recording which
+        # key is pressed
+
+        # e.g. if user presses "x" then exit  / press "f" for fullscreen
+        # display
+
+        if (key == ord('x')):
+            keep_processing = False
+        elif (key == ord('f')):
+            args.fullscreen = not(args.fullscreen)
+
+    # close all windows
+
+    cv2.destroyAllWindows()
+
+else:
+    print("No video file specified or camera connected.")
+
+##########################################################################
