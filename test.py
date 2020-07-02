ba32bfd1d159c17951627e5f83ee7394a5ee861c
Ryan <rlail1607@gmail.com>
2020-07-02 15:37:20 +0100

    pytest four test

--- test.py
+++ test.py
@@ -7,7 +7,26 @@ class TestClass:
         test_img = cv2.imread('test/samples/fig_2.png')
         test_saliency_mapper = SaliencyDoG()
         test_map = test_saliency_mapper.generate_saliency(test_img)
-        test_map_truth = cv2.imread('test/true_saliency_maps/fig_2_saliency.png')
-        test_map_truth = cv2.cvtColor(test_map_truth, cv2.COLOR_BGR2GRAY)
+        test_map_truth = cv2.imread('test/true_saliency_maps/fig_2_saliency.png', 0)
         assert np.array_equal(test_map, test_map_truth)
 
+    def test_two(self):
+        test_img = cv2.imread('test/samples/fig_2.png')
+        test_saliency_mapper = SaliencyDoG(ch_3=True)
+        test_map = test_saliency_mapper.generate_saliency(test_img)
+        test_map_truth = cv2.imread('test/true_saliency_maps/fig_2_saliency_3_ch.png', 0)
+        assert np.array_equal(test_map, test_map_truth)
+
+    def test_three(self):
+        test_img = cv2.imread('test/samples/fig_2.png')
+        test_saliency_mapper = SaliencyDoG(low_pass_filter=True)
+        test_map = test_saliency_mapper.generate_saliency(test_img)
+        test_map_truth = cv2.imread('test/true_saliency_maps/fig_2_saliency_lp.png', 0)
+        assert np.array_equal(test_map, test_map_truth)
+
+    def test_four(self):
+        test_img = cv2.imread('test/samples/fig_2.png')
+        test_saliency_mapper = SaliencyDoG(multi_layer_map=True)
+        test_map = test_saliency_mapper.generate_saliency(test_img)
+        test_map_truth = cv2.imread('test/true_saliency_maps/fig_2_saliency_mlm.png', 0)
+        assert np.array_equal(test_map, test_map_truth)
