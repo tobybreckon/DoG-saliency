a10155501b1eeb987456f24cb3fe7024c1616f62
Ryan Lail <me@ryanlail.com>
2020-07-14 15:14:44 +0100

    structure code

--- ransac_bounding_boxes.py
+++ ransac_bounding_boxes.py
@@ -8,6 +8,7 @@ import cv2
 import argparse
 import sys
 import math
+import random
 
 ##########################################################################
 
@@ -16,10 +17,77 @@ from saliencyDoG import SaliencyDoG
 ##########################################################################
 
 
-if __name__ == "__main__":
+def ransac_bounding_boxes(img, min_box=250, threashold=28, samples=100000,
+                          box_colour=(0,0,255), box_line_thickness=1):
 
-    keep_processing = True
-    toggle_saliency = True
+    # read in an image, generate it's saliency map and place bounding
+    # boxes
+
+    # min_box = minimum dimension of either side of box
+    # threashold = saliency density (saliency per pixel)
+    # samples = number of random samples to take
+    # box_colour = BGR tuple for box outine colour
+    # box_line_thickness = box outline thickness
+
+    # initialize saliency_mapper
+    saliency_mapper = SaliencyDoG()
+
+    # generate saliency map
+    saliency_map = saliency_mapper.generate_saliency(img)
+
+    # generate integral image
+    integral_image = cv2.integral(saliency_map)
+
+    # draw bounding boxes on original image
+    output = img
+
+    for box in range(samples):
+
+        # -1 as indexing starts at 0
+
+        x1 = random.randint(0, (integral_image.shape[1] - 1))
+        y1 = random.randint(0, (integral_image.shape[0] - 1))
+
+        x2 = random.randint(0, (integral_image.shape[1] - 1))
+        y2 = random.randint(0, (integral_image.shape[0] - 1))
+
+        if abs(x1-x2) < min_box or abs(y1-y2) < min_box:
+
+            continue
+
+        else:
+
+            # define points a,b,c,d in integral image to calculate
+            # saliency in box
+
+            xa = min(x1, x2)
+            ya = min(y1, y2)
+
+            xb = max(x1, x2)
+            yb = min(y1, y2)
+
+            xc = min(x1, x2)
+            yc = max(y1, y2)
+
+            xd = max(x1, x2)
+            yd = max(y1, y2)
+
+
+            box_saliency = (integral_image[yd][xd] - integral_image[yb][xb] -
+                    integral_image[yc][xc] + integral_image[ya][xa])
+
+            box_saliency_density = box_saliency / (abs(x1-x2) * abs(y1-y2))
+
+            if box_saliency_density > threashold:
+
+                output = cv2.rectangle(output, (x1, y1), (x2, y2),
+                                       box_colour, box_line_thickness)
+
+    return output
+
+##########################################################################
+
+if __name__ == "__main__":
 
     # parse command line arguments for camera ID or video file
 
@@ -37,64 +105,12 @@ if __name__ == "__main__":
 
     ##########################################################################
 
-    import random
-
-    # initialize saliency_mapper
-    saliency_mapper = SaliencyDoG()
 
     # read in image
     img = cv2.imread(args.image_file)
 
-    # generate saliency map
-    saliency_map = saliency_mapper.generate_saliency(img)
-
-    minimum_box = 5
-    maximum_box = 50
-    threashold = 20000
-
-    output = img
-
-    for scale in range(1, 4):
-
-        saliency_map_scaled = cv2.resize(saliency_map, (saliency_map.shape[1]//(2**scale), saliency_map.shape[0]//(2**scale)))
-
-        integral_image = cv2.integral(saliency_map_scaled)
-
-        for box in range(100000):
-
-            x1 = random.randint(0, (integral_image.shape[1] - 1))
-            y1 = random.randint(0, (integral_image.shape[0] - 1))
-
-            x2 = random.randint(0, (integral_image.shape[1] - 1))
-            y2 = random.randint(0, (integral_image.shape[0] - 1))
-
-            if abs(x1-x2) < minimum_box or abs(y1-y2) < minimum_box or abs(x1-x2) > maximum_box or abs(y1-y2) > maximum_box:
-
-                continue
-
-            else:
-
-
-                xa = min(x1, x2)
-                ya = min(y1, y2)
-
-                xb = max(x1, x2)
-                yb = min(y1, y2)
-
-                xc = min(x1, x2)
-                yc = max(y1, y2)
-
-                xd = max(x1, x2)
-                yd = max(y1, y2)
-
-
-                box_saliency = integral_image[yd][xd] - integral_image[yb][xb] - integral_image[yc][xc] + integral_image[ya][xa]
-
-                if box_saliency > threashold*(2**scale):
-
-                    print(box_saliency)
-                    output = cv2.rectangle(output, (x1*(2**scale), y1*(2**scale)), (x2*(2**scale), y2*(2**scale)), (255, 0, 0), 2)
-
+    # generate bounding boxes
+    output = ransac_bounding_boxes(img)
 
     cv2.imwrite("BoundingBoxes.png", output)
     cv2.waitKey(0)
