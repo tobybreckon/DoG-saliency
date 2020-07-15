6fd37943e3a83126f7f8c0528102becaddc0d8b8
Ryan Lail <me@ryanlail.com>
2020-07-15 16:52:44 +0100

    basic Non max suppression

--- ransac_bounding_boxes.py
+++ ransac_bounding_boxes.py
@@ -9,15 +9,61 @@ import argparse
 import sys
 import random
 import time
+import numpy as np
 
 ##########################################################################
 
 from saliencyDoG import SaliencyDoG
 
 ##########################################################################
+# Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+	# if there are no boxes, return an empty list
+	if len(boxes) == 0:
+		return []
+	# if the bounding boxes integers, convert them to floats --
+	# this is important since we'll be doing a bunch of divisions
+	if boxes.dtype.kind == "i":
+		boxes = boxes.astype("float")
+	# initialize the list of picked indexes
+	pick = []
+	# grab the coordinates of the bounding boxes
+	x1 = boxes[:,0]
+	y1 = boxes[:,1]
+	x2 = boxes[:,2]
+	y2 = boxes[:,3]
+	# compute the area of the bounding boxes and sort the bounding
+	# boxes by the bottom-right y-coordinate of the bounding box
+	area = (x2 - x1 + 1) * (y2 - y1 + 1)
+	idxs = np.argsort(y2)
+	# keep looping while some indexes still remain in the indexes
+	# list
+	while len(idxs) > 0:
+		# grab the last index in the indexes list and add the
+		# index value to the list of picked indexes
+		last = len(idxs) - 1
+		i = idxs[last]
+		pick.append(i)
+		# find the largest (x, y) coordinates for the start of
+		# the bounding box and the smallest (x, y) coordinates
+		# for the end of the bounding box
+		xx1 = np.maximum(x1[i], x1[idxs[:last]])
+		yy1 = np.maximum(y1[i], y1[idxs[:last]])
+		xx2 = np.minimum(x2[i], x2[idxs[:last]])
+		yy2 = np.minimum(y2[i], y2[idxs[:last]])
+		# compute the width and height of the bounding box
+		w = np.maximum(0, xx2 - xx1 + 1)
+		h = np.maximum(0, yy2 - yy1 + 1)
+		# compute the ratio of overlap
+		overlap = (w * h) / area[idxs[:last]]
+		# delete all indexes from the index list that have
+		idxs = np.delete(idxs, np.concatenate(([last],
+			np.where(overlap > overlapThresh)[0])))
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return boxes[pick].astype("int")
 
-
-def ransac_bounding_boxes(img, min_box=50, threashold=28, samples=10000,
+def ransac_bounding_boxes(img, min_box=50, threashold=40, samples=10000,
                           box_colour=(0, 0, 255), box_line_thickness=1):
 
     # read in an image, generate it's saliency map and place bounding
@@ -41,6 +87,9 @@ def ransac_bounding_boxes(img, min_box=50, threashold=28, samples=10000,
 
     # draw bounding boxes on original image
     output = img
+
+    boxes = []
+    confidences = []
 
     for box in range(samples):
 
@@ -79,9 +128,31 @@ def ransac_bounding_boxes(img, min_box=50, threashold=28, samples=10000,
             box_saliency_density = box_saliency / (abs(x1-x2) * abs(y1-y2))
 
             if box_saliency_density > threashold:
+                boxes.append([x1, y1, x2, y2])
 
-                output = cv2.rectangle(output, (x1, y1), (x2, y2),
-                                       box_colour, box_line_thickness)
+
+    indices = non_max_suppression_fast(np.asarray(boxes), 0.99)
+    print(indices)
+#            confidences.append(box_saliency_density)
+
+#    indices = cv2.dnn.NMSBoxes(boxes, confidences, threashold, 0.01)
+
+    for i in indices:
+#        i = i[0]
+#        box = boxes[i]
+        x1 = i[0]
+        y1 = i[1]
+        x2 = i[2]
+        y2 = i[3]
+
+        output = cv2.rectangle(output, (x1, y1), (x2, y2),
+                               box_colour, box_line_thickness)
+
+
+#            if box_saliency_density > threashold:
+
+#                output = cv2.rectangle(output, (x1, y1), (x2, y2),
+#                                       box_colour, box_line_thickness)
 
     return output
 
